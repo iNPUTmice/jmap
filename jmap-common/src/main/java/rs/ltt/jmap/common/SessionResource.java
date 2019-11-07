01685d30809531cd818eadc021fb6a4f9ae35e8f
cketti <ck@cketti.de>
2019-11-07 23:14:02 +0100

    Generate getters and toString() for SessionResource

--- jmap-common/src/main/java/rs/ltt/jmap/common/SessionResource.java
+++ jmap-common/src/main/java/rs/ltt/jmap/common/SessionResource.java
@@ -16,15 +16,15 @@
 
 package rs.ltt.jmap.common;
 
-import com.google.common.base.MoreObjects;
+import lombok.Getter;
+import lombok.ToString;
 import rs.ltt.jmap.common.entity.Account;
 import rs.ltt.jmap.common.entity.Capability;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Collection;
 import java.util.Map;
 
+@Getter
+@ToString
 public class SessionResource {
 
     private String username;
@@ -36,53 +36,7 @@ public class SessionResource {
     private Map<Class<?extends Capability>, Capability> capabilities;
     private String state;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public String getApiUrl() {
-        return this.apiUrl;
-    }
-
-    public String getDownloadUrl() {
-        return downloadUrl;
-    }
-
-    public String getUploadUrl() {
-        return uploadUrl;
-    }
-
-    public String getEventSourceUrl() {
-        return eventSourceUrl;
-    }
-
-    public Map<String, Account> getAccounts() {
-        return accounts;
-    }
-
     public <T extends Capability> T getCapability(Class<T> clazz) {
         return clazz.cast(capabilities.get(clazz));
-    }
-
-    public Collection<Capability> getCapabilities() {
-        return capabilities.values();
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    @Override
-    public String toString() {
-        return MoreObjects.toStringHelper(this)
-                .add("username", username)
-                .add("apiUrl", apiUrl)
-                .add("downloadUrl", downloadUrl)
-                .add("uploadUrl", uploadUrl)
-                .add("eventSourceUrl", eventSourceUrl)
-                .add("accounts", accounts)
-                .add("capabilities", capabilities)
-                .add("state", state)
-                .toString();
     }
 }
