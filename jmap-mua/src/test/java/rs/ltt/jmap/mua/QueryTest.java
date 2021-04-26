c69cdd6f1878b425170e1738aef3881b14ea1f81
Daniel Gultsch <daniel@gultsch.de>
2021-04-26 12:39:03 +0200

    jmap-mua: unit tests for query and query pagination

---
+++ jmap-mua/src/test/java/rs/ltt/jmap/mua/QueryTest.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2021 Daniel Gultsch
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package rs.ltt.jmap.mua;
+
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import rs.ltt.jmap.common.entity.query.EmailQuery;
+import rs.ltt.jmap.mock.server.JmapDispatcher;
+import rs.ltt.jmap.mock.server.MockMailServer;
+import rs.ltt.jmap.mua.cache.InMemoryCache;
+import rs.ltt.jmap.mua.util.QueryResultItem;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+public class QueryTest {
+
+    @Test
+    public void queryRefresh() throws IOException, InterruptedException, ExecutionException {
+        final MockMailServer mockMailServer = new MockMailServer(128);
+        final MockWebServer server = new MockWebServer();
+        server.setDispatcher(mockMailServer);
+
+        final MyInMemoryCache myInMemoryCache = new MyInMemoryCache();
+
+        final EmailQuery emailQuery = EmailQuery.unfiltered(true);
+
+        try (final Mua mua = Mua.builder()
+                .sessionResource(server.url(JmapDispatcher.WELL_KNOWN_PATH))
+                .cache(myInMemoryCache)
+                .username(mockMailServer.getUsername())
+                .password(JmapDispatcher.PASSWORD)
+                .accountId(mockMailServer.getAccountId())
+                .queryPageSize(5)
+                .build()) {
+
+            mua.query(emailQuery).get();
+
+            Assertions.assertEquals(
+                    Arrays.asList("T0", "T1", "T2", "T3", "T4"),
+                    myInMemoryCache.getThreadIdsInQuery(emailQuery.asHash())
+            );
+
+            mockMailServer.generateEmailOnTop();
+
+            mua.query(emailQuery).get();
+
+            final List<String> threadsAfterRefresh = myInMemoryCache.getThreadIdsInQuery(emailQuery.asHash());
+
+            Assertions.assertEquals(5, threadsAfterRefresh.size());
+
+            Assertions.assertEquals("T0", threadsAfterRefresh.get(1));
+            Assertions.assertEquals("T3", threadsAfterRefresh.get(4));
+
+
+        }
+
+        server.shutdown();
+    }
+
+    @Test
+    public void queryPagination() throws IOException, InterruptedException, ExecutionException {
+        final MockMailServer mockMailServer = new MockMailServer(128);
+        final MockWebServer server = new MockWebServer();
+        server.setDispatcher(mockMailServer);
+
+        final MyInMemoryCache myInMemoryCache = new MyInMemoryCache();
+
+        final EmailQuery emailQuery = EmailQuery.unfiltered(true);
+
+        try (final Mua mua = Mua.builder()
+                .sessionResource(server.url(JmapDispatcher.WELL_KNOWN_PATH))
+                .cache(myInMemoryCache)
+                .username(mockMailServer.getUsername())
+                .password(JmapDispatcher.PASSWORD)
+                .accountId(mockMailServer.getAccountId())
+                .queryPageSize(5)
+                .build()) {
+
+            mua.query(emailQuery).get();
+
+            Assertions.assertEquals(
+                    Arrays.asList("T0", "T1", "T2", "T3", "T4"),
+                    myInMemoryCache.getThreadIdsInQuery(emailQuery.asHash())
+            );
+
+            final String lastEmailId = myInMemoryCache.getItems(emailQuery.asHash()).get(4).getEmailId();
+
+            Assertions.assertEquals("M10", lastEmailId);
+
+
+            mua.query(emailQuery, lastEmailId).get();
+
+            Assertions.assertEquals(
+                    Arrays.asList("T0", "T1", "T2", "T3", "T4","T5","T6","T7","T8","T9"),
+                    myInMemoryCache.getThreadIdsInQuery(emailQuery.asHash())
+            );
+        }
+
+        server.shutdown();
+    }
+
+    //TODO coping queryPagination with a followed up refresh() is a good opportunity to test calculateQueryPageSizet
+
+    private static class MyInMemoryCache extends InMemoryCache {
+
+        public List<String> getThreadIdsInQuery(final String queryHash) {
+            return getItems(queryHash).stream().map(QueryResultItem::getThreadId).collect(Collectors.toList());
+        }
+
+        public List<QueryResultItem> getItems(final String queryHash) {
+            final InMemoryQueryResult queryResult = queryResults.get(queryHash);
+            return queryResult.getItems();
+        }
+
+    }
+}
