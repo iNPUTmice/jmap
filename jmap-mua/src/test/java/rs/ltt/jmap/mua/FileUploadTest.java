d00cc049b383fbe2a9bd01871d6897bcb02b9cf6
Daniel Gultsch <daniel@gultsch.de>
2021-12-20 11:12:50 +0100

    test getMediaType on Upload class

--- jmap-mua/src/test/java/rs/ltt/jmap/mua/FileUploadTest.java
+++ jmap-mua/src/test/java/rs/ltt/jmap/mua/FileUploadTest.java
@@ -107,6 +107,8 @@ public class FileUploadTest {
         Assertions.assertEquals(
                 "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9",
                 upload.getBlobId());
+        Assertions.assertEquals("text/plain", upload.getType());
+        Assertions.assertTrue(MediaType.create("text", "plain").is(upload.getMediaType()));
     }
 
     @Test
